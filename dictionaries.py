5206c3799c9271caeedfeb144f5373c3230b0080
Nobody-UwU <sahrohan183@gmail.comm>
2021-02-17 15:58:40 +0530

    trying formatting

--- dictionaries.py
+++ dictionaries.py
@@ -1,16 +1,16 @@
 cardetail = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
 }
 print(cardetail["brand"])
 print(len(cardetail))
 print(type(cardetail))
 x = cardetail.keys()
-y= cardetail.values()
-z= cardetail.items()
-print(x,y,z)
-cardetail["color"]="white"
-print(x,y,z)
+y = cardetail.values()
+z = cardetail.items()
+print(x, y, z)
+cardetail["color"] = "white"
+print(x, y, z)
 if "brand" in cardetail:
-  print("Yes, 'model' is one of the keys in the cardetails dictionary")
+    print("Yes, 'model' is one of the keys in the cardetails dictionary")
